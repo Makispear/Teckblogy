fd853e0bc5fb2340e21784374778ea24942c33b4
Maki Aboabida <maki-miko@hotmail.com>
2021-10-23 22:33:05 -0400

    Added: Api folder

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const sequelize = require('../config/connection')
 const { Post, User, Comment } = require('../models')
+const post404Message = 'Post with this id not found'
 
 
 router.get('/', (req, res) => {
@@ -24,7 +25,48 @@ router.get('/', (req, res) => {
     })
     .catch(err => {
         console.log(err)
-        res.status(500).json(err)
+        return res.status(500).json(err)
+    })
+})
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'text', 'user_id', 'post_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            return res.status(404).json({message: post404Message})
+        }
+        const post = dbPostData.get({plain: true})
+        res.render('single-post', {
+            post
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        return res.status(500).json(err)
     })
 })
 
