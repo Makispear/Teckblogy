ca9b46294dac6aa4e5964a67f0f50aa3270b5bce
Maki Aboabida <maki-miko@hotmail.com>
2021-10-30 01:10:54 -0400

    Added: time limit for session

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: process.env.SECRET_WORD,
-  cookie: {},
+  cookie: { maxAge: 3600 * 1000},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
